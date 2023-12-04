1a07bbbaa501b05b72d071aedccd9f8836bf7f09
Eric Park <me@ericswpark.com>
2023-12-04 08:33:06 -0500

    2023: day 4: refactor

--- 2023/day-04/src/main.rs
+++ 2023/day-04/src/main.rs
@@ -43,21 +43,13 @@ fn get_points_worth(input: &Vec<String>) -> i32 {
         let parts: Vec<_> = line.split(": ").collect();
         // let card_id = parts[0][5..].parse::<i32>().unwrap();
         let numbers: Vec<_> = parts[1].split(" | ").collect();
-        let winning_numbers: Vec<i32> = numbers[0].split(' ').filter(|&x| !x.is_empty()).map(|s: &str| s.parse::<i32>().unwrap() ).collect();
-        let elf_numbers: Vec<i32> = numbers[1].split(' ').filter(|&x| !x.is_empty()).map(|s: &str| s.parse::<i32>().unwrap() ).collect();
+        let winning_numbers: Vec<i32> = parse_input_to_scratchcard_numbers(numbers[0]);
+        let elf_numbers: Vec<i32> = parse_input_to_scratchcard_numbers(numbers[1]);
 
         // Get number of matches
-        let mut match_count = 0;
-        for i in 0..elf_numbers.len() {
-            for j in 0..winning_numbers.len() {
-                if elf_numbers.get(i) == winning_numbers.get(j) {
-                    match_count += 1;
-                    break;
-                }
-            }
-        }
+        let match_count = get_match_count(winning_numbers, elf_numbers);
 
-        points_sum += if match_count >= 1 { 2_i32.checked_pow(match_count - 1).unwrap() } else { 0 };
+        points_sum += if match_count >= 1 { 2_i32.checked_pow((match_count - 1) as u32).unwrap() } else { 0 };
     }
     points_sum
 }
@@ -69,21 +61,13 @@ fn get_card_instances_sum(input: &Vec<String>) -> i32 {
         let parts: Vec<_> = line.split(": ").collect();
         let card_id = parts[0][5..].trim().parse::<i32>().unwrap();
         let numbers: Vec<_> = parts[1].split(" | ").collect();
-        let winning_numbers: Vec<i32> = numbers[0].split(' ').filter(|&x| !x.is_empty()).map(|s: &str| s.parse::<i32>().unwrap() ).collect();
-        let elf_numbers: Vec<i32> = numbers[1].split(' ').filter(|&x| !x.is_empty()).map(|s: &str| s.parse::<i32>().unwrap() ).collect();
+        let winning_numbers: Vec<i32> = parse_input_to_scratchcard_numbers(numbers[0]);
+        let elf_numbers: Vec<i32> = parse_input_to_scratchcard_numbers(numbers[1]);
 
         cards_count_by_id[card_id as usize - 1] += 1;
 
         // Get number of matches
-        let mut match_count = 0;
-        for i in 0..elf_numbers.len() {
-            for j in 0..winning_numbers.len() {
-                if elf_numbers.get(i) == winning_numbers.get(j) {
-                    match_count += 1;
-                    break;
-                }
-            }
-        }
+        let match_count = get_match_count(winning_numbers, elf_numbers);
 
         // Increment number of matches, repeat for number of matches in current ID
         for _ in 0..cards_count_by_id[card_id as usize - 1] {
@@ -96,6 +80,19 @@ fn get_card_instances_sum(input: &Vec<String>) -> i32 {
     cards_count_by_id.iter().sum()
 }
 
+fn get_match_count(winning_numbers: Vec<i32>, elf_numbers: Vec<i32>) -> usize {
+    let mut match_count = 0;
+    for i in 0..elf_numbers.len() {
+        for j in 0..winning_numbers.len() {
+            if elf_numbers.get(i) == winning_numbers.get(j) {
+                match_count += 1;
+                break;
+            }
+        }
+    }
+    match_count
+}
+
 fn get_card_count(input: &Vec<String>) -> i32 {
     let mut last_card_id = 0;
     for line in input {
@@ -104,4 +101,8 @@ fn get_card_count(input: &Vec<String>) -> i32 {
     }
 
     last_card_id
+}
+
+fn parse_input_to_scratchcard_numbers(input: &str) -> Vec<i32> {
+    input.split(' ').filter(|&x| !x.is_empty()).map(|s: &str| s.parse::<i32>().unwrap() ).collect()
 }

7d1fcdd535eaf3da77882f3a59c2387fa5139f62
Eric Park <me@ericswpark.com>
2023-12-07 11:55:18 -0500

    2023: day 7: finish part 2

--- 2023/day-07/src/main.rs
+++ 2023/day-07/src/main.rs
@@ -64,13 +64,54 @@ impl Round {
             _ => panic!("Bad hand type mapping. The input is wrong!")
         }
     }
+
+    fn hand_type_with_joker(&self) -> HandType {
+        let mut cards: Vec<i16> = vec![0; 13];
+        for hand in &self.hands {
+            cards[get_card_value_mapping_with_joker(*hand) as usize] += 1;
+        }
+
+        // Back up joker count for now
+        let joker_count = cards[0];
+        cards[0] = 0;
+
+        cards.sort();
+        cards.reverse();
+
+        // Add joker count to the highest card number
+        cards[0] += joker_count;
+
+        match cards[0] {
+            5 => FiveKind,
+            4 => FourKind,
+            3 => {
+                match cards[1] {
+                    2 => FullHouse,
+                    1 => ThreeKind,
+                    _ => panic!("Impossible three-card mapping. The input is wrong!")
+                }
+            },
+            2 => {
+                match cards[1] {
+                    2 => TwoPair,
+                    1 => OnePair,
+                    _ => panic!("Impossible two-card mapping. The input is wrong!")
+                }
+            },
+            1 => HighCard,
+            _ => panic!("Bad hand type mapping. The input is wrong!")
+        }
+    }
 }
 
 fn main() {
     let input = helpers::get_input(&*helpers::get_path_from_arg());
 
     let part_1_answer = part_1(&input);
-    println!("Part 1 answer: {part_1_answer}.");
+    println!("Part 1 answer: {part_1_answer}");
+
+    let part_2_answer = part_2(&input);
+    println!("Part 2 answer: {part_2_answer}");
 }
 
 fn part_1(input: &Vec<String>) -> i64 {
@@ -82,6 +123,31 @@ fn part_1(input: &Vec<String>) -> i64 {
         rounds.sort_by(|a, b| get_card_value_mapping(a.hands[4-i]).cmp(&get_card_value_mapping(b.hands[4 - i])))
     }
     rounds.sort_by(|a, b | a.hand_type().value().cmp(&b.hand_type().value()));
+
+    let mut total_winnings: i64 = 0;
+
+    for (round_index, round) in rounds.iter().enumerate() {
+        total_winnings += ((round_index + 1) as i32 * round.bid) as i64;
+    }
+
+    total_winnings
+}
+
+fn part_2(input: &Vec<String>) -> i64 {
+    let mut rounds = get_card_rounds(input);
+
+    // Sort rounds based on their rank
+    // If two rounds have the same rank, then use the individual card values
+    for i in 0..=4 {
+        rounds.sort_by(|a, b|
+            get_card_value_mapping_with_joker(a.hands[4-i])
+                .cmp(&get_card_value_mapping_with_joker(b.hands[4 - i]))
+        )
+    }
+    rounds.sort_by(|a, b |
+        a.hand_type_with_joker().value()
+            .cmp(&b.hand_type_with_joker().value())
+    );
 
     let mut total_winnings: i64 = 0;
 
@@ -128,6 +194,25 @@ fn get_card_value_mapping(c: char) -> i16 {
         '4' => 2,
         '3' => 1,
         '2' => 0,
+        _ => { panic!("Bad card mapping!") }
+    }
+}
+
+fn get_card_value_mapping_with_joker(c: char) -> i16 {
+    match c {
+        'A' => 12,
+        'K' => 11,
+        'Q' => 10,
+        'T' => 9,
+        '9' => 8,
+        '8' => 7,
+        '7' => 6,
+        '6' => 5,
+        '5' => 4,
+        '4' => 3,
+        '3' => 2,
+        '2' => 1,
+        'J' => 0,
         _ => { panic!("Bad card mapping!") }
     }
 }
--- 2023/day-07/src/tests.rs
+++ 2023/day-07/src/tests.rs
@@ -1,18 +1,26 @@
 #[cfg(test)]
 mod tests {
     use crate::helpers::get_input;
-    use crate::part_1;
+    use crate::{part_1, part_2};
 
     #[test]
     fn reddit_sostratus() {
         let input = get_input("reddit-test-case/Sostratus.txt");
         assert_eq!(part_1(&input), 1343);
-        //assert_eq!(part_2(&input), 1369);
+        assert_eq!(part_2(&input), 1369);
     }
 
     #[test]
     fn sample_input() {
         let input = get_input("sample-input.txt");
         assert_eq!(part_1(&input), 6440);
+        assert_eq!(part_2(&input), 5905);
+    }
+
+    #[test]
+    fn puzzle_input() {
+        let input = get_input("puzzle-input.txt");
+        assert_eq!(part_1(&input), 248105065);
+        assert_eq!(part_2(&input), 249515436);
     }
 }

a1858cb4b63a69a52f91b34e91dea41afd9ac423
Eric Park <me@ericswpark.com>
2023-12-07 12:18:59 -0500

    2023: day 7: add test case from Reddit

--- 2023/day-07/reddit-test-case/README.md
+++ 2023/day-07/reddit-test-case/README.md
@@ -3,3 +3,7 @@
 ## u/Sostratus
 
 https://www.reddit.com/r/adventofcode/comments/18cq5j3/2023_day_7_part_1_two_hints_for_anyone_stuck/
+
+## u/LxsterGames
+
+https://www.reddit.com/r/adventofcode/comments/18cr4xr/2023_day_7_better_example_input_not_a_spoiler/
--- 2023/day-07/src/tests.rs
+++ 2023/day-07/src/tests.rs
@@ -11,6 +11,13 @@ mod tests {
     }
 
     #[test]
+    fn reddit_lxstergames() {
+        let input = get_input("reddit-test-case/LxsterGames.txt");
+        assert_eq!(part_1(&input), 6592);
+        assert_eq!(part_2(&input), 6839);
+    }
+
+    #[test]
     fn sample_input() {
         let input = get_input("sample-input.txt");
         assert_eq!(part_1(&input), 6440);

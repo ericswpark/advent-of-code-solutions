e526791819f9496b14966608df02239b640912ac
Eric Park <me@ericswpark.com>
2023-12-08 10:41:49 -0500

    2023: day 8: cache position of Vec index
    Should speed up runtime

--- 2023/day-08/src/main.rs
+++ 2023/day-08/src/main.rs
@@ -4,7 +4,17 @@ mod helpers;
 struct MapNode {
     name: String,
     left: String,
-    right: String
+    right: String,
+    left_index: Option<i32>,
+    right_index: Option<i32>,
+}
+
+impl MapNode {
+    fn new(name: String, left: String, right: String) -> Self {
+        MapNode {
+            name, left, right, left_index: None, right_index: None
+        }
+    }
 }
 
 fn main() {
@@ -20,18 +30,15 @@ fn main() {
 fn part_1(input: &Vec<String>) -> i64 {
     let sequence = get_sequence(&input[0]);
 
-    let map: Vec<MapNode> = get_map(&input[2..]);
+    let mut map: Vec<MapNode> = get_map(&input[2..]);
 
     let mut current_node_index = 0;
     let mut current_sequence_index = 0;
     let mut count = 0;
 
     loop {
-        // Get current node
-        let current = &map[current_node_index];
-
         // If current node is ZZZ, break
-        if current.name == "ZZZ" { break }
+        if map[current_node_index].name == "ZZZ" { break }
 
         // Go to the next node based on the current sequence
         let next_direction = sequence[current_sequence_index];
@@ -39,23 +46,34 @@ fn part_1(input: &Vec<String>) -> i64 {
 
         match next_direction {
             'L' => {
-                let next_node_name = &current.left;
-                current_node_index = map.iter().position(|r| r.name == *next_node_name).unwrap();
+                let next_node_name = &map[current_node_index].left;
+
+                if map[current_node_index].left_index == None {
+                    map[current_node_index].left_index = Some(map.iter().position(|r| r.name == *next_node_name).unwrap() as i32);
+                }
+
+                current_node_index = map[current_node_index].left_index.unwrap() as usize;
+
             },
             'R' => {
-                let next_node_name = &current.right;
-                current_node_index = map.iter().position(|r| r.name == *next_node_name).unwrap();
+                let next_node_name = &map[current_node_index].right;
+
+                if map[current_node_index].right_index == None {
+                    map[current_node_index].right_index = Some(map.iter().position(|r| r.name == *next_node_name).unwrap() as i32);
+                }
+
+                current_node_index = map[current_node_index].right_index.unwrap() as usize;
             },
             _ => panic!("Bad sequence!"),
         }
 
-        count += 1
+        count += 1;
     }
 
     count
 }
 
-fn part_2(input: &Vec<String>) -> i64 {
+fn part_2(_input: &Vec<String>) -> i64 {
     0
 }
 
@@ -73,11 +91,11 @@ fn get_map(input: &[String]) -> Vec<MapNode> {
         let left_name = &next_direction[1..=3];
         let right_name = &next_direction[6..=8];
 
-        map.push(MapNode{
-            name: start_name.parse().unwrap(),
-            left: left_name.parse().unwrap(),
-            right: right_name.parse().unwrap()
-        });
+        map.push(MapNode::new(
+            start_name.parse().unwrap(),
+            left_name.parse().unwrap(),
+            right_name.parse().unwrap()
+        ));
     }
 
     map

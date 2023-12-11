606898569ec4d75a29d1894ae207fd063d88a317
Eric Park <me@ericswpark.com>
2023-12-11 10:00:07 -0500

    2023: day 11: finish part 2

--- 2023/day-11/src/tests.rs
+++ 2023/day-11/src/tests.rs
@@ -13,6 +13,6 @@ mod tests {
     fn puzzle_input() {
         let input = get_input("puzzle-input.txt");
         assert_eq!(part_1(&input), 9403026);
-        //assert_eq!(part_2(&input), 337);
+        assert_eq!(part_2(&input), 543018317006);
     }
 }

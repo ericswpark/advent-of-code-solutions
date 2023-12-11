ac2ea7300a790482100c99c475579fc33ec99c92
Eric Park <me@ericswpark.com>
2023-12-11 10:16:27 -0500

    2015: day 1: finish part 1
    Trying some old puzzles

---
+++ 2015/day-01/src/main.rs
@@ -0,0 +1,34 @@
+mod helpers;
+mod tests;
+
+fn main() {
+    let input = helpers::get_input(&*helpers::get_path_from_arg());
+
+    let part_1_answer = part_1(&input);
+    println!("Part 1 answer: {part_1_answer}");
+
+    let part_2_answer = part_2(&input);
+    println!("Part 2 answer: {part_2_answer}");
+}
+
+
+fn part_1(input: &Vec<String>) -> i32 {
+    let input = &input[0];
+
+    get_level(input)
+}
+
+
+
+fn part_2(input: &Vec<String>) -> i64 {
+    todo!()
+}
+
+fn get_level(input: &String) -> i32{
+    let mut level = 0;
+    for c in input.chars() {
+        if c == '(' { level += 1 }
+        else if c == ')' { level -= 1 }
+    }
+    level
+}
---
+++ 2015/day-01/src/tests.rs
@@ -0,0 +1,68 @@
+#[cfg(test)]
+mod tests {
+    use crate::helpers::get_input;
+    use crate::{part_1, part_2};
+
+    #[test]
+    fn sample_input_1() {
+        let input = get_input("sample-input-1.txt");
+        assert_eq!(part_1(&input), 0);
+    }
+
+    #[test]
+    fn sample_input_2() {
+        let input = get_input("sample-input-2.txt");
+        assert_eq!(part_1(&input), 0);
+    }
+
+    #[test]
+    fn sample_input_3() {
+        let input = get_input("sample-input-3.txt");
+        assert_eq!(part_1(&input), 3);
+    }
+
+    #[test]
+    fn sample_input_4() {
+        let input = get_input("sample-input-4.txt");
+        assert_eq!(part_1(&input), 3);
+    }
+
+    #[test]
+    fn sample_input_5() {
+        let input = get_input("sample-input-5.txt");
+        assert_eq!(part_1(&input), 3);
+    }
+
+    #[test]
+    fn sample_input_6() {
+        let input = get_input("sample-input-6.txt");
+        assert_eq!(part_1(&input), -1);
+    }
+
+    #[test]
+    fn sample_input_7() {
+        let input = get_input("sample-input-7.txt");
+        assert_eq!(part_1(&input), -1);
+    }
+
+    #[test]
+    fn sample_input_8() {
+        let input = get_input("sample-input-8.txt");
+        assert_eq!(part_1(&input), -3);
+    }
+
+    #[test]
+    fn sample_input_9() {
+        let input = get_input("sample-input-9.txt");
+        assert_eq!(part_1(&input), -3);
+    }
+
+
+
+    #[test]
+    fn puzzle_input() {
+        let input = get_input("puzzle-input.txt");
+        assert_eq!(part_1(&input), 232);
+        // assert_eq!(part_2(&input), 543018317006);
+    }
+}

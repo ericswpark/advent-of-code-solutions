e158c4f60a154188978aa2606e3a3b40fdebb82c
Eric Park <me@ericswpark.com>
2023-12-12 11:11:32 -0500

    2023: day 12: start

---
+++ 2023/day-12/src/main.rs
@@ -0,0 +1,23 @@
+mod helpers;
+mod tests;
+
+fn main() {
+    let input = helpers::get_input(&*helpers::get_path_from_arg());
+
+    let part_1_answer = part_1(&input);
+    println!("Part 1 answer: {part_1_answer}");
+
+    let part_2_answer = part_2(&input);
+    println!("Part 2 answer: {part_2_answer}");
+}
+
+
+fn part_1(input: &Vec<String>) -> i32 {
+    todo!()
+}
+
+
+
+fn part_2(input: &Vec<String>) -> i64 {
+    todo!()
+}

4857d36f81afc0400a914b907123c72a9ddbe62a
Eric Park <me@ericswpark.com>
2023-12-12 14:50:14 -0500

    2023: day 12: partially finish part 1
    It passes all the sample tests but the actual puzzle answer is too high

--- 2023/day-12/src/main.rs
+++ 2023/day-12/src/main.rs
@@ -1,3 +1,5 @@
+use crate::Condition::{Damaged, Operational, Unknown};
+
 mod helpers;
 mod tests;
 
@@ -13,11 +15,125 @@ fn main() {
 
 
 fn part_1(input: &Vec<String>) -> i32 {
-    todo!()
+    let field = parse_field(input);
+
+    let mut arrangement_sum = 0;
+
+    for row in field {
+        arrangement_sum += get_arrangements(row);
+    }
+
+    arrangement_sum
 }
 
 
 
 fn part_2(input: &Vec<String>) -> i64 {
     todo!()
+}
+
+
+#[derive(PartialEq, Clone)]
+enum Condition {
+    Operational,
+    Damaged,
+    Unknown,
+}
+
+#[derive(Clone)]
+struct Row {
+    springs: Vec<Condition>,
+    damaged_spring_groups: Vec<i32>,
+}
+
+impl Condition {
+    fn mapping(c: char) -> Self {
+        match c {
+            '.' => Operational,
+            '#' => Damaged,
+            '?' => Unknown,
+            _ => panic!("Bad condition character!"),
+        }
+    }
+}
+
+fn parse_field(input: &Vec<String>) -> Vec<Row> {
+    let mut rows = Vec::new();
+
+    for line in input {
+        rows.push(parse_row(line));
+    }
+
+    rows
+}
+
+fn parse_row(input: &String) -> Row {
+    let parts: Vec<String> = input.split(' ').map(|s| s.to_string()).collect();
+    let mut springs = Vec::new();
+
+    for c in parts[0].chars() {
+        springs.push(Condition::mapping(c));
+    }
+
+    let damaged_spring_groups: Vec<i32> = parts[1].split(',').map(|s| s.parse::<i32>().unwrap()).collect();
+
+    Row { springs, damaged_spring_groups }
+}
+
+fn get_arrangements(row: Row) -> i32 {
+    row_analyze(&row)
+}
+
+fn row_analyze(row: &Row) -> i32 {
+    let mut row = row.clone();
+
+    // No springs left
+    if row.springs.len() == 0 {
+        // Check if we used up all the damaged spring groups as well
+        return if row.damaged_spring_groups.len() != 0 { 0 } else { 1 }
+    }
+
+    // No damaged blocks left
+    if row.damaged_spring_groups.len() == 0 { return 1; }
+
+    // Match with the first spring
+    match row.springs[0] {
+        Operational => {
+            row.springs.remove(0);
+            row_analyze(&row)
+        },
+        Unknown => {
+            row.springs[0] = Operational;
+            let mut result = row_analyze(&row);
+            row.springs[0] = Damaged;
+            result += row_analyze(&row);
+            result
+        },
+        Damaged => {
+            // Check if we have enough springs left
+            if row.springs.len() < row.damaged_spring_groups[0].try_into().unwrap() {
+                return 0;
+            }
+
+            // Check contiguous block
+            for spring_index in 0..row.damaged_spring_groups[0] {
+                if row.springs[spring_index as usize] == Operational { return 0; }
+            }
+
+            // If the next index after the block exists, make sure it's not damaged
+            if ((row.damaged_spring_groups[0] + 1) as usize) <= row.springs.len() {
+                if row.springs[row.damaged_spring_groups[0] as usize] == Damaged { return 0; }
+                // If the next index is unknown, set it to operational (must be)
+                else if row.springs[row.damaged_spring_groups[0] as usize] == Unknown {
+                    row.springs[row.damaged_spring_groups[0] as usize] = Operational;
+                }
+            }
+
+            // Remove the block and springs
+            row.springs.drain(0..row.damaged_spring_groups[0] as usize);
+            row.damaged_spring_groups.remove(0);
+
+            row_analyze(&row)
+        },
+    }
 }
--- 2023/day-12/src/tests.rs
+++ 2023/day-12/src/tests.rs
@@ -1,18 +1,55 @@
 #[cfg(test)]
 mod tests {
     use crate::helpers::get_input;
-    use crate::{part_1, part_2};
+    use crate::*;
+
+    #[test]
+    fn arrangement_test_1() {
+        let row = parse_row(&String::from("???.### 1,1,3"));
+        assert_eq!(get_arrangements(row), 1);
+    }
+
+    #[test]
+    fn arrangement_test_2() {
+        let row = parse_row(&String::from(".??..??...?##. 1,1,3"));
+        assert_eq!(get_arrangements(row), 4);
+    }
+
+    #[test]
+    fn arrangement_test_3() {
+        let row = parse_row(&String::from("?#?#?#?#?#?#?#? 1,3,1,6"));
+        assert_eq!(get_arrangements(row), 1);
+    }
+
+    #[test]
+    fn arrangement_test_4() {
+        let row = parse_row(&String::from("????.#...#... 4,1,1"));
+        assert_eq!(get_arrangements(row), 1);
+    }
+
+    #[test]
+    fn arrangement_test_5() {
+        let row = parse_row(&String::from("????.######..#####. 1,6,5"));
+        assert_eq!(get_arrangements(row), 4);
+    }
+
+    #[test]
+    fn arrangement_test_6() {
+        let row = parse_row(&String::from("?###???????? 3,2,1"));
+        assert_eq!(get_arrangements(row), 10);
+    }
 
     #[test]
     fn sample_input() {
         let input = get_input("sample-input.txt");
-        assert_eq!(part_1(&input), 374);
+        assert_eq!(part_1(&input), 21);
     }
+
 
     #[test]
     fn puzzle_input() {
         let input = get_input("puzzle-input.txt");
-        assert_eq!(part_1(&input), 9403026);
-        assert_eq!(part_2(&input), 543018317006);
+        //assert_eq!(part_1(&input), 7975);
+        //assert_eq!(part_2(&input), 543018317006);
     }
 }

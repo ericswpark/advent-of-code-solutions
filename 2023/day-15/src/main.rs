d619534b1a2ee986660f5aba5cab668f984535f3
Eric Park <me@ericswpark.com>
2023-12-15 00:53:30 -0500

    2023: day 15: finish part 2

--- 2023/day-15/src/main.rs
+++ 2023/day-15/src/main.rs
@@ -22,10 +22,80 @@ fn part_1(input: &Vec<String>) -> i64 {
     sum
 }
 
+#[derive(Clone)]
+struct LensBox {
+    lenses: Vec<Lens>
+}
 
+#[derive(Clone)]
+struct Lens {
+    label: String,
+    focal_length: u8,
+}
 
 fn part_2(input: &Vec<String>) -> i64 {
-    todo!()
+    // Create 0~255 lens boxes
+    let mut lens_boxes: Vec<LensBox> = vec![LensBox { lenses: Vec::new() }; 256];
+
+    for step in input {
+        let (label, operation, focal_length) = parse_step(step);
+
+        let box_num = hash_algo(&label) as usize;
+
+        match operation {
+            Operation::Set => {
+                let mut found_lens = false;
+                for lens in &mut lens_boxes[box_num].lenses {
+                    if lens.label == label {
+                        found_lens = true;
+                        lens.focal_length = focal_length;
+                        break;
+                    }
+                }
+                if !found_lens {
+                    lens_boxes[box_num].lenses.push(Lens { label, focal_length })
+                }
+            }
+            Operation::Remove => {
+                for (index, lens) in lens_boxes[box_num].lenses.iter().enumerate() {
+                    if lens.label == label {
+                        lens_boxes[box_num].lenses.remove(index);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    let mut focusing_power_sum: i64 = 0;
+
+    for (index, lens_box) in lens_boxes.iter().enumerate() {
+        focusing_power_sum += calculate_focus_power(lens_box, index) as i64;
+    }
+
+    focusing_power_sum
+}
+
+fn calculate_focus_power(lens_box: &LensBox, box_index: usize) -> i32 {
+    let mut sum: i32 = 0;
+    for (lens_index, lens) in lens_box.lenses.iter().enumerate() {
+        sum += ((box_index + 1) * (lens_index + 1) * lens.focal_length as usize) as i32;
+    }
+    sum
+}
+
+enum Operation {
+    Set,
+    Remove,
+}
+
+fn parse_step(input: &String) -> (String, Operation, u8) {
+    return if !input.chars().nth(input.len() - 1).unwrap().is_numeric() {
+        ((&input[0..input.len() - 1]).to_string(), Operation::Remove, 0)
+    } else {
+        let focal_length: u8 = input.chars().nth(input.len() - 1).unwrap().to_digit(10).unwrap() as u8;
+        ((&input[0..input.len() - 2]).to_string(), Operation::Set, focal_length)
+    }
 }
 
 
--- 2023/day-15/src/tests.rs
+++ 2023/day-15/src/tests.rs
@@ -7,6 +7,7 @@ mod tests {
     fn sample_input() {
         let input = get_input("sample-input.txt");
         assert_eq!(part_1(&input), 1320);
+        assert_eq!(part_2(&input), 145);
     }
 
 
@@ -14,6 +15,6 @@ mod tests {
     fn puzzle_input() {
         let input = get_input("puzzle-input.txt");
         assert_eq!(part_1(&input), 501680);
-        //assert_eq!(part_2(&input), 102055);
+        assert_eq!(part_2(&input), 241094);
     }
 }

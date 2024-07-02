1edf77ba3fdcee8dbb50260317aab94000210ca6
Eric Park <me@ericswpark.com>
2024-07-02 10:30:59 +0900

    2023: day 17: add test case by u/1234abcdcba4321

---
+++ 2023/day-17/reddit-test-cases/README.md
@@ -0,0 +1,6 @@
+# Reddit test cases
+
+## u/1234abcdcba4321
+
+[https://www.reddit.com/r/adventofcode/comments/1dswqh3/2023_day_17_part_1_rustlang_answer_too_high_but/lb6d7qm/](https://www.reddit.com/r/adventofcode/comments/1dswqh3/2023_day_17_part_1_rustlang_answer_too_high_but/lb6d7qm/)
+
--- 2023/day-17/src/tests.rs
+++ 2023/day-17/src/tests.rs
@@ -3,6 +3,13 @@ mod tests {
     use crate::helpers::get_input;
     use crate::*;
 
+    //noinspection SpellCheckingInspection
+    #[test]
+    fn reddit_1234abcdcba4321() {
+        let input = get_input("reddit-test-cases/1234abcdcba4321.txt");
+        assert_eq!(part_1(&input), 12);
+    }
+
     #[test]
     fn sample_input() {
         let input = get_input("sample-input.txt");

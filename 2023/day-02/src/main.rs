3f3a56fd9f3d4e6a0a8a22a9d94959c5217b8971
Eric Park <me@ericswpark.com>
2024-12-01 16:05:54 -0500

    2023: day 2: split main into part 1 and 2

--- 2023/day-02/src/main.rs
+++ 2023/day-02/src/main.rs
@@ -29,15 +29,43 @@ fn get_color_mapping(s: &str) -> Color {
 fn main() {
     let input = get_input(&get_path_from_arg());
 
+    let part_1_answer = part_1(&input);
+    println!("Part 1 answer: {part_1_answer}");
+
+    let part_2_answer = part_2(&input);
+    println!("Part 2 answer: {part_2_answer}");
+}
+
+fn part_1(input: &[String]) -> u32 {
     let mut game_id_sum: u32 = 0;
+
+    for line in input {
+        let parts: Vec<_> = line.split(": ").collect();
+        let game_id: u32 = parts[0][5..].parse().unwrap();
+        let mut is_possible = true;
+        let reveal_sets: Vec<_> = parts[1].split("; ").collect();
+
+        for reveal_set in reveal_sets {
+            let blocks: Vec<_> = reveal_set.split(", ").collect();
+            for block in blocks {
+                let data: Vec<_> = block.split(' ').collect();
+                let count: u32 = data[0].parse().unwrap();
+                let color: Color = get_color_mapping(data[1]);
+
+                if count > color.minimums() as u32 { is_possible = false }
+            }
+        }
+        if is_possible { game_id_sum += game_id }
+    }
+    game_id_sum
+}
+
+fn part_2(input: &[String]) -> u32 {
     let mut power_sum = 0;
 
     for line in input {
         let parts: Vec<_> = line.split(": ").collect();
 
-        let game_id: u32 = parts[0][5..].parse().unwrap();
-
-        let mut is_possible = true;
         let mut required_red = 0;
         let mut required_green = 0;
         let mut required_blue = 0;
@@ -50,8 +78,6 @@ fn main() {
                 let data: Vec<_> = block.split(' ').collect();
                 let count: u32 = data[0].parse().unwrap();
                 let color: Color = get_color_mapping(data[1]);
-
-                if count > color.minimums() as u32 { is_possible = false }
 
                 match color {
                     Color::RED => {
@@ -68,10 +94,7 @@ fn main() {
 
 
         }
-        if is_possible { game_id_sum += game_id }
         power_sum += required_red * required_green * required_blue;
     }
-
-    println!("The sum of the game IDs is {game_id_sum}");
-    println!("The sum of the cube powers is {power_sum}")
+    power_sum
 }

915b531e703a8e40ff90b3408c2aa1970b2b52dd
Eric Park <me@ericswpark.com>
2024-12-01 18:10:34 -0500

    2023: day 17: fix Ord and PartialOrd implementation
    Apparently, letting Rust derive PartialOrd when I already have an
    explicit Ord implementation is bad.

--- 2023/day-17/src/structs.rs
+++ 2023/day-17/src/structs.rs
@@ -2,7 +2,7 @@ use std::cmp::Ordering;
 
 use crate::enums::Direction;
 
-#[derive(Copy, Clone, PartialEq, Hash, Eq, PartialOrd)]
+#[derive(Copy, Clone, PartialEq, Hash, Eq)]
 pub(crate) struct Coordinate {
     pub(crate) x: usize,
     pub(crate) y: usize,
@@ -24,6 +24,12 @@ pub(crate) struct Iteration {
 impl Ord for Coordinate {
     fn cmp(&self, other: &Self) -> Ordering {
         self.x.cmp(&other.x).then_with(|| self.y.cmp(&other.y))
+    }
+}
+
+impl PartialOrd for Coordinate {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
 }
 

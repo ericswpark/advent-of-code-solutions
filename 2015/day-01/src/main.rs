d7c0b7968da275f0d4668ad7a37a0d24e109aa7f
Eric Park <me@ericswpark.com>
2024-12-01 15:47:58 -0500

    2015: day 1: clippy fix

--- 2015/day-01/src/main.rs
+++ 2015/day-01/src/main.rs
@@ -12,19 +12,19 @@ fn main() {
     println!("Part 2 answer: {part_2_answer}");
 }
 
-fn part_1(input: &Vec<String>) -> i32 {
+fn part_1(input: &[String]) -> i32 {
     let input = &input[0];
 
     get_level(input)
 }
 
-fn part_2(input: &Vec<String>) -> usize {
+fn part_2(input: &[String]) -> usize {
     let input = &input[0];
 
     get_basement_position(input) + 1
 }
 
-fn get_level(input: &String) -> i32 {
+fn get_level(input: &str) -> i32 {
     let mut level = 0;
     for c in input.chars() {
         if c == '(' {
@@ -36,7 +36,7 @@ fn get_level(input: &String) -> i32 {
     level
 }
 
-fn get_basement_position(input: &String) -> usize {
+fn get_basement_position(input: &str) -> usize {
     let mut level = 0;
     for (index, c) in input.chars().enumerate() {
         if c == '(' {

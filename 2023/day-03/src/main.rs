f55c8ab05dea4c592084b5d1620a0f863ec691fd
Eric Park <me@ericswpark.com>
2024-12-01 16:14:42 -0500

    2023: day 3: use helper library

--- 2023/day-03/src/main.rs
+++ 2023/day-03/src/main.rs
@@ -1,41 +1,13 @@
-use std::env;
-use std::fs;
-use std::io::{stdin, stdout, Write};
+use helpers::*;
 
 fn main() {
-    let input = get_input();
+    let input = get_input(&get_path_from_arg()).iter().map(|s| s.chars().collect()).collect();
 
     let sum = get_part_sum(&input);
     let gear_ratio_sum = get_gear_ratio_sum(&input);
 
     println!("The part number sum is {sum}.");
     println!("The gear ratio sum is {gear_ratio_sum}.");
-}
-
-fn get_input() -> Vec<Vec<char>> {
-    let mut args: Vec<String> = env::args().collect();
-
-    let mut path: &mut String = &mut String::new();
-
-    if args.len() < 2 {
-        print!("Enter path to file: ");
-        stdout().flush().expect("Cannot flush buffer");
-
-        stdin().read_line(path).expect("Cannot process input");
-        if let Some('\n')=path.chars().next_back() {
-            path.pop();
-        }
-        if let Some('\r')=path.chars().next_back() {
-            path.pop();
-        }
-    } else {
-        path = &mut args[1];
-    }
-
-    fs::read_to_string(path)
-        .expect("Couldn't read input file").split('\n')
-        .map(|s: &str| s.chars().collect())
-        .collect()
 }
 
 fn get_gear_ratio_sum(input: &Vec<Vec<char>>) -> i32 {

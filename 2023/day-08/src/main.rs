7d36fe3d5a1ca380d16f714dc4dbfaa3fa2bf8a7
Eric Park <me@ericswpark.com>
2024-12-01 17:19:46 -0500

    2023: day 8: refactor to use helper lib

--- 2023/day-08/src/main.rs
+++ 2023/day-08/src/main.rs
@@ -1,5 +1,6 @@
-mod helpers;
 mod tests;
+
+use helpers::*;
 
 
 struct MapNode {
@@ -19,7 +20,7 @@ impl MapNode {
 }
 
 fn main() {
-    let input = helpers::get_input(&*helpers::get_path_from_arg());
+    let input = get_input(&get_path_from_arg());
 
     let part_1_answer = part_1(&input);
     println!("Part 1 answer: {part_1_answer}");

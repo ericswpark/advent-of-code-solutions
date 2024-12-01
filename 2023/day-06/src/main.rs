a28507e898b6350c5cdb89ab1c45b5f117c4f0ba
Eric Park <me@ericswpark.com>
2024-12-01 17:14:32 -0500

    2023: day 6: refactor to use helper lib

--- 2023/day-06/src/main.rs
+++ 2023/day-06/src/main.rs
@@ -1,8 +1,6 @@
 mod tests;
 
-use std::env;
-use std::fs;
-use std::io::{stdin, stdout, Write};
+use helpers::*;
 
 
 fn main() {
@@ -56,33 +54,4 @@ fn get_parsed_number_array(input: &str) -> Vec<i64> {
 
 fn calculate_distance(total: i64, charge_up: i64) -> i64 {
     charge_up * (total - charge_up)
-}
-
-fn get_path_from_arg() -> String {
-    let mut args: Vec<String> = env::args().collect();
-
-    let mut path: &mut String = &mut String::new();
-
-    if args.len() < 2 {
-        print!("Enter path to file: ");
-        stdout().flush().expect("Cannot flush buffer");
-
-        stdin().read_line(path).expect("Cannot process input");
-        if let Some('\n')=path.chars().next_back() {
-            path.pop();
-        }
-        if let Some('\r')=path.chars().next_back() {
-            path.pop();
-        }
-    } else {
-        path = &mut args[1];
-    }
-
-    path.to_owned()
-}
-
-
-fn get_input(path: &str) -> Vec<String> {
-    fs::read_to_string(path)
-        .expect("Couldn't read input file").split("\n").map(|s| s.to_string()).collect()
 }

e831e64ea4c8a12c63beac3382f21090ef3775b6
Eric Park <me@ericswpark.com>
2024-12-01 15:30:53 -0500

    2023: day 5: use helper import
    Fixes crashing on Windows

--- 2023/day-05/src/main.rs
+++ 2023/day-05/src/main.rs
@@ -1,8 +1,7 @@
-use std::env;
-use std::fs;
-use std::io::{stdin, stdout, Write};
 use rayon::prelude::*;
 use std::time::Instant;
+
+use helpers::*;
 
 struct RangeItem {
     dest_start: i64,
@@ -51,7 +50,7 @@ struct RangeSeed {
 }
 
 fn main() {
-    let input = get_input();
+    let input = get_input(&get_path_from_arg());
 
     let start_time = Instant::now();
 
@@ -141,28 +140,4 @@ fn get_mapping(input: &String) -> RangeVec {
     }
 
     map
-}
-
-fn get_input() -> Vec<String> {
-    let mut args: Vec<String> = env::args().collect();
-
-    let mut path: &mut String = &mut String::new();
-
-    if args.len() < 2 {
-        print!("Enter path to file: ");
-        stdout().flush().expect("Cannot flush buffer");
-
-        stdin().read_line(path).expect("Cannot process input");
-        if let Some('\n')=path.chars().next_back() {
-            path.pop();
-        }
-        if let Some('\r')=path.chars().next_back() {
-            path.pop();
-        }
-    } else {
-        path = &mut args[1];
-    }
-
-    fs::read_to_string(path)
-        .expect("Couldn't read input file").split("\n\n").map(|s| s.to_string()).collect()
 }

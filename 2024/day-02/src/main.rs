54272569e1f963da6eebfa06d765e1d361c8fd59
Eric Park <me@ericswpark.com>
2024-12-02 00:22:24 -0500

    2024: day 2: finish part 1

--- 2024/day-02/src/main.rs
+++ 2024/day-02/src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::time::Instant;
 
 use helpers::*;
@@ -19,7 +18,41 @@ fn main() {
 }
 
 fn part_1(input: &Vec<String>) -> i64 {
-    todo!()
+    let mut safe_count = 0;
+
+    for report in input {
+        let levels: Vec<u32> = report.split(' ').map(|s| s.parse::<u32>().unwrap()).collect();
+
+        // Check if strictly increasing or decreasing
+        let mut sorted: Vec<u32> = levels.clone();
+        sorted.sort();
+        let mut sorted_reverse = levels.clone();
+        sorted_reverse.sort_by(|a, b| b.cmp(a));
+
+        if !(levels == sorted || levels == sorted_reverse) {
+            // Not safe, not strictly increasing or decreasing
+            continue;
+        }
+
+        let mut is_safe = true;
+
+        for index in 0..levels.len() - 1 {
+            let current = levels[index];
+            let next = levels[index + 1];
+            let diff = current.abs_diff(next);
+
+            if diff <= 0 || diff > 3 {
+                is_safe = false;
+                break;
+            }
+        }
+
+        if is_safe {
+            safe_count += 1;
+        }
+    }
+
+    safe_count
 }
 
 fn part_2(input: &Vec<String>) -> i64 {
--- 2024/day-02/src/tests.rs
+++ 2024/day-02/src/tests.rs
@@ -6,14 +6,14 @@ mod tests {
     #[test]
     fn sample_input() {
         let input = get_input("sample-input.txt");
-        assert_eq!(part_1(&input), 11);
-        assert_eq!(part_2(&input), 31);
+        assert_eq!(part_1(&input), 2);
+        //assert_eq!(part_2(&input), 31);
     }
 
     #[test]
     fn puzzle_input() {
         let input = get_input("puzzle-input.txt");
-        assert_eq!(part_1(&input), 2378066);
-        assert_eq!(part_2(&input), 18934359);
+        assert_eq!(part_1(&input), 236);
+        //assert_eq!(part_2(&input), );
     }
 }

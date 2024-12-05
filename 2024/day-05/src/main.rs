c889269c275369f424ed063ad428e7f62a8ead45
Eric Park <me@ericswpark.com>
2024-12-05 00:36:29 -0500

    2024: day 5: finish part 1

--- 2024/day-05/src/main.rs
+++ 2024/day-05/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::time::Instant;
 
 use helpers::*;
@@ -18,9 +19,78 @@ fn main() {
 }
 
 fn part_1(input: &Vec<String>) -> i64 {
-    todo!()
+    let (page_ordering_rules, update_page_numbers) = parse_input(input);
+    let correct_updates = get_correct_updates(&page_ordering_rules, &update_page_numbers);
+
+    println!("{:?}", correct_updates);
+    let mut sum = 0;
+    for update in correct_updates {
+        sum += get_middle(&update);
+    }
+    sum
 }
 
 fn part_2(input: &Vec<String>) -> i64 {
     todo!()
+}
+
+
+fn parse_input(input: &[String]) -> (HashMap<i64, Vec<i64>>, Vec<Vec<i64>>) {
+    let mut page_ordering_rules: HashMap<i64, Vec<i64>> = HashMap::new();
+    let mut update_page_numbers = Vec::new();
+
+    let mut is_on_next_section = false;
+    for line in input {
+        if line == "" {
+            is_on_next_section = true;
+            continue;
+        }
+
+        if !is_on_next_section {
+            let [a, b] = line.split('|').map(|x| x.parse::<i64>().unwrap()).collect::<Vec<_>>().try_into().unwrap();
+            if let Some(next_pages) = page_ordering_rules.get_mut(&a) {
+                next_pages.push(b);
+            } else {
+                page_ordering_rules.insert(a, vec![b]);
+            }
+            continue;
+        }
+
+        let current_update_page_numbers = line.split(',').map(|x| x.parse::<i64>().unwrap()).collect::<Vec<i64>>();
+        update_page_numbers.push(current_update_page_numbers);
+    }
+
+    (page_ordering_rules, update_page_numbers)
+}
+
+fn get_correct_updates(page_ordering_rules: &HashMap<i64, Vec<i64>>, update_page_numbers: &[Vec<i64>]) -> Vec<Vec<i64>> {
+    let mut correct_updates = Vec::new();
+
+    for update in update_page_numbers {
+        if update_is_correct(page_ordering_rules, update) {
+            correct_updates.push(update.clone());
+        }
+    }
+
+    correct_updates
+}
+
+fn update_is_correct(page_ordering_rules: &HashMap<i64, Vec<i64>>, update: &[i64]) -> bool {
+    for (index, num) in update.iter().enumerate().rev() {
+        if let Some(next_pages) = page_ordering_rules.get(num) {
+            for f_index in 0..index {
+                let prev_page = update[f_index];
+                if next_pages.contains(&prev_page) {
+                    return false;
+                }
+            }
+        }
+        continue;
+    }
+
+    true
+}
+
+fn get_middle(update: &[i64]) -> i64 {
+    update[update.len() / 2]
 }
--- 2024/day-05/src/tests.rs
+++ 2024/day-05/src/tests.rs
@@ -6,14 +6,14 @@ mod tests {
     #[test]
     fn sample_input() {
         let input = get_input("sample-input.txt");
-        assert_eq!(part_1(&input), 18);
+        assert_eq!(part_1(&input), 143);
         //assert_eq!(part_2(&input), 9);
     }
 
     #[test]
     fn puzzle_input() {
         let input = get_input("puzzle-input.txt");
-        assert_eq!(part_1(&input), 2654);
+        assert_eq!(part_1(&input), 7365);
         //assert_eq!(part_2(&input), 1990);
     }
 }

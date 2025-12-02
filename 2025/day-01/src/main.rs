aa955e177db04760ec6ea5e54699b12890911002
Eric Park <me@ericswpark.com>
2025-12-02 12:04:48 -0500

    2025: day 1: finish part 2

--- 2025/day-01/src/main.rs
+++ 2025/day-01/src/main.rs
@@ -48,5 +48,40 @@ fn part_1(input: &[String]) -> i64 {
 }
 
 fn part_2(input: &[String]) -> i64 {
-    todo!()
+    let mut current = 50;
+    let mut password = 0;
+
+    for line in input {
+        let direction = line.chars().next().unwrap();
+        let mut distance = line[1..].parse::<i64>().unwrap();
+        match direction {
+            'L' => {
+                while distance > 0 {
+                    current -= 1;
+                    if current == -1 {
+                        current = 99
+                    };
+                    if current == 0 {
+                        password += 1;
+                    }
+                    distance -= 1;
+                }
+            }
+            'R' => {
+                while distance > 0 {
+                    current += 1;
+                    if current == 100 {
+                        current = 0
+                    };
+                    if current == 0 {
+                        password += 1;
+                    }
+                    distance -= 1;
+                }
+            }
+            _ => unreachable!("Shouldn't happen, direction is either L or R"),
+        }
+    }
+
+    password
 }
--- 2025/day-01/src/tests.rs
+++ 2025/day-01/src/tests.rs
@@ -7,13 +7,62 @@ mod tests {
     fn sample_input() {
         let input = get_input("sample-input.txt");
         assert_eq!(part_1(&input), 3);
-        //assert_eq!(part_2(&input), 11387);
+        assert_eq!(part_2(&input), 6);
     }
 
     #[test]
     fn puzzle_input() {
         let input = get_input("puzzle-input.txt");
         assert_eq!(part_1(&input), 1007);
-        //assert_eq!(part_2(&input), 1026766857276279);
+        assert_eq!(part_2(&input), 5820);
+    }
+
+    #[test]
+    fn multiple_rotations_at_once() {
+        let input = [String::from("R1000")];
+        assert_eq!(part_1(&input), 0);
+        assert_eq!(part_2(&input), 10);
+    }
+
+    #[test]
+    fn rotate_to_zero() {
+        let input = [String::from("L50")];
+        assert_eq!(part_1(&input), 1);
+        assert_eq!(part_2(&input), 1);
+
+        let input = [String::from("L50"), String::from("R100")];
+        assert_eq!(part_1(&input), 2);
+        assert_eq!(part_2(&input), 2);
+
+        let input = [
+            String::from("L50"),
+            String::from("R100"),
+            String::from("R200"),
+        ];
+        assert_eq!(part_1(&input), 3);
+        assert_eq!(part_2(&input), 4);
+
+        let input = [
+            String::from("L50"),
+            String::from("R100"),
+            String::from("L300"),
+        ];
+        assert_eq!(part_1(&input), 3);
+        assert_eq!(part_2(&input), 5);
+    }
+
+    #[test]
+    fn boundaries() {
+        let input = [String::from("L49")];
+        assert_eq!(part_1(&input), 0);
+        assert_eq!(part_2(&input), 0);
+
+        let input = [String::from("L49"), String::from("L1")];
+        assert_eq!(part_1(&input), 1);
+        assert_eq!(part_2(&input), 1);
+
+        let input = [String::from("L49"), String::from("L2")];
+        assert_eq!(part_1(&input), 0);
+        assert_eq!(part_2(&input), 1);
     }
 }

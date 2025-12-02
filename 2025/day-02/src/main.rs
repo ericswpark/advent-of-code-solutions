7e8baf0af81f64d35c23203f5b57214a1b8c9cb3
Eric Park <me@ericswpark.com>
2025-12-02 12:27:39 -0500

    2025: day 2: finish part 1

--- 2025/day-02/src/main.rs
+++ 2025/day-02/src/main.rs
@@ -18,8 +18,56 @@ fn main() {
     println!("Time: {:.2?}", elapsed_time);
 }
 
+#[derive(Debug, Clone, Copy)]
+struct Range {
+    start: i64,
+    end: i64,
+}
+
 fn part_1(input: &[String]) -> i64 {
-    todo!()
+    let mut answer = 0;
+    let ranges = input[0]
+        .split(',')
+        .map(|range| {
+            let (start, end) = range
+                .split_once('-')
+                .map(|(start, end)| (start.parse().unwrap(), end.parse().unwrap()))
+                .unwrap();
+            Range { start, end }
+        })
+        .collect::<Vec<Range>>();
+
+    for range in ranges {
+        for number in range.start..=range.end {
+            if !check_validity(number) {
+                answer += number;
+            }
+        }
+    }
+
+    answer
+}
+
+fn check_validity(number: i64) -> bool {
+    let number_string = number.to_string();
+
+    // If the length is not even, it cannot repeat
+    if number_string.len() % 2 != 0 {
+        return true;
+    }
+
+    for index in 0..(number_string.len() / 2) {
+        if number_string.chars().nth(index).unwrap()
+            != number_string
+                .chars()
+                .nth(number_string.len() / 2 + index)
+                .unwrap()
+        {
+            return true;
+        }
+    }
+
+    false
 }
 
 fn part_2(input: &[String]) -> i64 {

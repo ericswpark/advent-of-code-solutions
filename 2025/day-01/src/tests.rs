91fd5cde70eb239097b5b396f21181d6d76799f7
Eric Park <me@ericswpark.com>
2025-12-02 11:02:50 -0500

    2025: day 1: start

---
+++ 2025/day-01/src/tests.rs
@@ -0,0 +1,19 @@
+#[cfg(test)]
+mod tests {
+    use crate::*;
+    use helpers::get_input;
+
+    #[test]
+    fn sample_input() {
+        let input = get_input("sample-input.txt");
+        assert_eq!(part_1(&input), 3);
+        //assert_eq!(part_2(&input), 11387);
+    }
+
+    #[test]
+    fn puzzle_input() {
+        let input = get_input("puzzle-input.txt");
+        //assert_eq!(part_1(&input), 42283209483350);
+        //assert_eq!(part_2(&input), 1026766857276279);
+    }
+}

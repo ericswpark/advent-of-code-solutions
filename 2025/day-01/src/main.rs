52021e4f66dcd4e3a33a418da95d3e0196445c6c
Eric Park <me@ericswpark.com>
2025-12-02 11:10:13 -0500

    2025: day 1: finish part 1

--- 2025/day-01/src/main.rs
+++ 2025/day-01/src/main.rs
@@ -19,10 +19,34 @@ fn main() {
 }
 
 fn part_1(input: &[String]) -> i64 {
-    todo!()
+    let mut current = 50;
+    let mut password = 0;
+
+    for line in input {
+        let direction = line.chars().next().unwrap();
+        let distance = line[1..].parse::<i64>().unwrap();
+        match direction {
+            'L' => current += distance,
+            'R' => current -= distance,
+            _ => unreachable!("Shouldn't happen, direction is either L or R"),
+        }
+
+        // Loop around
+        while current > 99 {
+            current -= 100;
+        }
+        while current < 0 {
+            current += 100;
+        }
+
+        if current == 0 {
+            password += 1;
+        }
+    }
+
+    password
 }
 
 fn part_2(input: &[String]) -> i64 {
     todo!()
 }
-
--- 2025/day-01/src/tests.rs
+++ 2025/day-01/src/tests.rs
@@ -13,7 +13,7 @@ mod tests {
     #[test]
     fn puzzle_input() {
         let input = get_input("puzzle-input.txt");
-        //assert_eq!(part_1(&input), 42283209483350);
+        assert_eq!(part_1(&input), 1007);
         //assert_eq!(part_2(&input), 1026766857276279);
     }
 }

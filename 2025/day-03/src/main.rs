ab7f16b1204863ca15747b813a12954a10d001b0
Eric Park <me@ericswpark.com>
2025-12-03 15:19:08 -0500

    2025: day 3: rewrite to use optimized get_max_joltage function only

--- 2025/day-03/src/main.rs
+++ 2025/day-03/src/main.rs
@@ -18,35 +18,16 @@ fn main() {
     println!("Time: {:.2?}", elapsed_time);
 }
 
-fn get_max_joltage(bank: &str) -> i64 {
-    let mut max_joltage = 0;
-    for first_digit_index in 0..bank.len() - 1 {
-        for second_digit_index in first_digit_index + 1..bank.len() {
-            let joltage = format!(
-                "{}{}",
-                bank.chars().nth(first_digit_index).unwrap(),
-                bank.chars().nth(second_digit_index).unwrap()
-            )
-            .parse::<i64>()
-            .unwrap();
-            if joltage > max_joltage {
-                max_joltage = joltage;
-            }
-        }
-    }
-    max_joltage
-}
-
 fn part_1(input: &[String]) -> i64 {
     let mut answer = 0;
     for line in input {
-        let max_joltage = get_max_joltage(line);
+        let max_joltage = get_max_joltage(line, 2);
         answer += max_joltage;
     }
     answer
 }
 
-fn get_max_twelve_batteries_joltage(bank: &str) -> i64 {
+fn get_max_joltage(bank: &str, battery_count: usize) -> i64 {
     // Split bank into individual batteries
     let batteries: Vec<u8> = bank
         .chars()
@@ -56,9 +37,9 @@ fn get_max_twelve_batteries_joltage(bank: &str) -> i64 {
     let mut from_index = 0;
     let mut joltage = 0;
 
-    while batteries_used < 12 {
+    while batteries_used < battery_count {
         // Get range of batteries we can consider
-        let batteries_needed_next_iter = 12 - batteries_used - 1;
+        let batteries_needed_next_iter = battery_count - batteries_used - 1;
         let up_to_index = batteries.len() - 1 - batteries_needed_next_iter;
 
         // Find the biggest battery within the range
@@ -91,7 +72,7 @@ fn get_max_twelve_batteries_joltage(bank: &str) -> i64 {
 fn part_2(input: &[String]) -> i64 {
     let mut answer = 0;
     for line in input {
-        let max_joltage = get_max_twelve_batteries_joltage(line);
+        let max_joltage = get_max_joltage(line, 12);
         answer += max_joltage;
     }
     answer

ac21c13edafffc76f225760edc4cd8b6f18fcc5f
Eric Park <me@ericswpark.com>
2025-12-03 12:10:06 -0500

    2025: day 3: failed part 1 attempt
    Too low, also doesn't work because there may be a better value if we stop earlier

--- 2025/day-03/src/main.rs
+++ 2025/day-03/src/main.rs
@@ -18,8 +18,55 @@ fn main() {
     println!("Time: {:.2?}", elapsed_time);
 }
 
+fn get_max_joltage(bank: &str) -> i64 {
+    let mut first_digit_index = bank.len() - 2;
+    let mut second_digit_index = bank.len() - 1;
+
+    // Figure out how big the first digit can be
+    for index in (0..first_digit_index - 1).rev() {
+        if bank.chars().nth(index).unwrap().to_digit(10).unwrap()
+            > bank
+                .chars()
+                .nth(first_digit_index)
+                .unwrap()
+                .to_digit(10)
+                .unwrap()
+        {
+            first_digit_index = index;
+        }
+    }
+
+    // Figure out how big the second digit can be
+    for index in (first_digit_index + 1..second_digit_index - 1).rev() {
+        if bank.chars().nth(index).unwrap().to_digit(10).unwrap()
+            > bank
+                .chars()
+                .nth(second_digit_index)
+                .unwrap()
+                .to_digit(10)
+                .unwrap()
+        {
+            second_digit_index = index;
+        }
+    }
+
+    // Return parsed integer
+    format!(
+        "{}{}",
+        bank.chars().nth(first_digit_index).unwrap(),
+        bank.chars().nth(second_digit_index).unwrap()
+    )
+    .parse()
+    .unwrap()
+}
+
 fn part_1(input: &[String]) -> i64 {
-    todo!();
+    let mut answer = 0;
+    for line in input {
+        let max_joltage = get_max_joltage(line);
+        answer += max_joltage;
+    }
+    answer
 }
 
 fn part_2(input: &[String]) -> i64 {
--- 2025/day-03/src/tests.rs
+++ 2025/day-03/src/tests.rs
@@ -13,7 +13,7 @@ mod tests {
     #[test]
     fn puzzle_input() {
         let input = get_input("puzzle-input.txt");
-        //assert_eq!(part_1(&input), 21139440284);
+        assert_eq!(part_1(&input), 17329);
         //assert_eq!(part_2(&input), 38731915928);
     }
 }

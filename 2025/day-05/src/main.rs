e9178d7dd67e26acf5d976656dde28966e628c1a
Eric Park <me@ericswpark.com>
2025-12-05 12:31:15 -0500

    2025: day 5: finish part 1
    Don't think the range merge optimization was necessary but /shrug

--- 2025/day-05/src/main.rs
+++ 2025/day-05/src/main.rs
@@ -19,9 +19,97 @@ fn main() {
 }
 
 fn part_1(input: &[String]) -> i64 {
-    todo!();
+    let data = parse_input(input);
+
+    // Merge ranges
+    let merged_ranges = merge_ranges(&data.ranges);
+
+    let mut count = 0;
+
+    for ingredient in data.available {
+        for range in &merged_ranges {
+            if range.includes(ingredient) {
+                count += 1;
+                break;
+            }
+        }
+    }
+
+    count
 }
 
 fn part_2(input: &[String]) -> i64 {
     todo!();
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Range {
+    start: i64,
+    end: i64,
+}
+
+impl Range {
+    fn includes(&self, value: i64) -> bool {
+        self.start <= value && value <= self.end
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Data {
+    ranges: Vec<Range>,
+    available: Vec<i64>,
+}
+
+fn parse_input(input: &[String]) -> Data {
+    let mut ranges = Vec::new();
+    let mut available = Vec::new();
+    let mut is_on_ranges = true;
+
+    for line in input {
+        if line == "" {
+            is_on_ranges = false;
+            continue;
+        }
+
+        match is_on_ranges {
+            true => {
+                let (start, end) = line.split_once('-').unwrap();
+                let range = Range {
+                    start: start.parse().unwrap(),
+                    end: end.parse().unwrap(),
+                };
+                ranges.push(range);
+            }
+            false => {
+                let num = line.parse().unwrap();
+                available.push(num);
+            }
+        }
+    }
+
+    Data { ranges, available }
+}
+
+fn merge_ranges(ranges: &[Range]) -> Vec<Range> {
+    let mut merged_ranges: Vec<Range> = Vec::new();
+
+    for range in ranges {
+        let mut merged = false;
+
+        // Check if range start or end is in any merged range
+        for merged_range in merged_ranges.iter_mut() {
+            if merged_range.includes(range.start) || merged_range.includes(range.end) {
+                merged_range.start = merged_range.start.min(range.start);
+                merged_range.end = merged_range.end.max(range.end);
+                merged = true;
+                break;
+            }
+        }
+
+        if !merged {
+            merged_ranges.push(*range);
+        }
+    }
+
+    merged_ranges
 }
--- 2025/day-05/src/tests.rs
+++ 2025/day-05/src/tests.rs
@@ -13,7 +13,14 @@ mod tests {
     #[test]
     fn puzzle_input() {
         let input = get_input("puzzle-input.txt");
-        //assert_eq!(part_1(&input), 1516);
+        assert_eq!(part_1(&input), 613);
         //assert_eq!(part_2(&input), 9122);
+    }
+
+    #[test]
+    fn merge_range_check() {
+        let ranges = vec![Range { start: 1, end: 5 }, Range { start: 2, end: 10 }];
+        let merged_ranges = vec![Range { start: 1, end: 10 }];
+        assert_eq!(merge_ranges(&ranges), merged_ranges);
     }
 }

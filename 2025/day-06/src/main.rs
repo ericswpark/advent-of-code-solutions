1410c471b9835d077e76abab1475c3035320bed6
Eric Park <me@ericswpark.com>
2025-12-08 13:33:05 -0500

    2025: day 6: finish part 2

--- 2025/day-06/src/main.rs
+++ 2025/day-06/src/main.rs
@@ -50,7 +50,76 @@ fn part_1(input: &[String]) -> i64 {
 }
 
 fn part_2(input: &[String]) -> i64 {
-    todo!();
+    let mut operations = Vec::new();
+
+    // Go through the last line and figure out the indices for operations
+    for (index, ch) in input.last().unwrap().chars().enumerate() {
+        match ch {
+            '+' => operations.push(Operation {
+                operation_type: OperationType::Add,
+                start_index: index,
+            }),
+            '*' => operations.push(Operation {
+                operation_type: OperationType::Multiply,
+                start_index: index,
+            }),
+            ' ' => continue,
+            _ => unreachable!("Unsupported operation"),
+        }
+    }
+
+    // For each operation, get the corresponding numbers and perform calculation
+    operations
+        .iter()
+        .enumerate()
+        .map(|(index, operation)| {
+            let next_operation = operations.get(index + 1);
+            let mut end_index = input.iter().map(|line| line.len()).max().unwrap();
+
+            // If there is a next operation
+            if let Some(next_operation) = next_operation {
+                // Set the ending index to one right before
+                end_index = next_operation.start_index - 1;
+            }
+
+            // Iterate backwards and grab numbers
+            let numbers: Vec<i64> = (operation.start_index..end_index)
+                .map(|index| {
+                    // Get each full number and add to operations
+                    input.iter().take(input.len() - 1).fold(0i64, |acc, line| {
+                        let ch = line.chars().nth(index);
+                        if let Some(ch) = ch
+                            && ch != ' '
+                        {
+                            let number = ch.to_digit(10).unwrap() as i64;
+                            return acc * 10 + number;
+                        }
+                        acc
+                    })
+                })
+                .collect::<Vec<i64>>();
+
+            println!("Numbers: {:?}", numbers);
+
+            // Perform calculation based on operation type
+            match operation.operation_type {
+                OperationType::Add => numbers.iter().sum::<i64>(),
+                OperationType::Multiply => numbers.iter().product(),
+            }
+        })
+        .sum()
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Operation {
+    operation_type: OperationType,
+    start_index: usize,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum OperationType {
+    Add,
+    Multiply,
 }
 
 fn parse_to_arrays(input: &[String]) -> Vec<Vec<String>> {
--- 2025/day-06/src/tests.rs
+++ 2025/day-06/src/tests.rs
@@ -7,13 +7,13 @@ mod tests {
     fn sample_input() {
         let input = get_input("sample-input.txt");
         assert_eq!(part_1(&input), 4277556);
-        //assert_eq!(part_2(&input), 14);
+        assert_eq!(part_2(&input), 3263827);
     }
 
     #[test]
     fn puzzle_input() {
         let input = get_input("puzzle-input.txt");
         assert_eq!(part_1(&input), 5335495999141);
-        //assert_eq!(part_2(&input), 336495597913098);
+        assert_eq!(part_2(&input), 10142723156431);
     }
 }

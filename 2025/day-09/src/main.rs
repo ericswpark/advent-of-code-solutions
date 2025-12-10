9a6b6cb6bf267303ad6bbdeb04f3e150fafa4cf2
Eric Park <me@ericswpark.com>
2025-12-10 00:09:26 -0500

    2025: day 9: refactor a bit

--- 2025/day-09/src/main.rs
+++ 2025/day-09/src/main.rs
@@ -44,38 +44,37 @@ fn part_2(input: &[String]) -> i64 {
     points
         .iter()
         .enumerate()
-        .map(|(index, &point)| {
+        .map(|(index, &point1)| {
             points
                 .iter()
                 .skip(index + 1)
-                .map(|&other_point| {
-                    let pair1 = Point {
-                        x: point.x,
-                        y: other_point.y,
+                .map(|&point2| {
+                    let point3 = Point {
+                        x: point1.x,
+                        y: point2.y,
                     };
-                    let pair2 = Point {
-                        x: other_point.x,
-                        y: point.y,
+                    let point4 = Point {
+                        x: point2.x,
+                        y: point1.y,
                     };
                     // Find connecting point
-                    if seen_points.contains(&pair1) {
+                    if seen_points.contains(&point3) {
                         // See if there is some point further away than the other pair
-                        if (pair1.x > pair2.x
-                            && pair1.y > pair2.y
-                            && points.iter().any(|&p| p.x <= pair2.x && p.y <= pair2.y))
-                            || (pair1.x > pair2.x
-                                && pair1.y < pair2.y
-                                && points.iter().any(|&p| p.x <= pair2.x && p.y >= pair2.y))
-                            || (pair1.x < pair2.x
-                                && pair1.y > pair2.y
-                                && points.iter().any(|&p| p.x >= pair2.x && p.y <= pair2.y))
-                            || (pair1.x < pair2.x
-                                && pair1.y < pair2.y
-                                && points.iter().any(|&p| p.x >= pair2.x && p.y >= pair2.y))
+                        if (point3.x > point4.x
+                            && point3.y > point4.y
+                            && points.iter().any(|&p| p.x <= point4.x && p.y <= point4.y))
+                            || (point3.x > point4.x
+                                && point3.y < point4.y
+                                && points.iter().any(|&p| p.x <= point4.x && p.y >= point4.y))
+                            || (point3.x < point4.x
+                                && point3.y > point4.y
+                                && points.iter().any(|&p| p.x >= point4.x && p.y <= point4.y))
+                            || (point3.x < point4.x
+                                && point3.y < point4.y
+                                && points.iter().any(|&p| p.x >= point4.x && p.y >= point4.y))
                         {
                             // Check if there are any points within the rectangle formed
-                            let x_range = pair1.x.min(pair2.x) + 1..pair1.x.max(pair2.x);
-                            let y_range = pair1.y.min(pair2.y) + 1..pair1.y.max(pair2.y);
+                            let (x_range, y_range) = get_ranges(&point3, &point4);
                             if points
                                 .iter()
                                 .any(|&check_point| within_range(&x_range, &y_range, &check_point))
@@ -84,26 +83,25 @@ fn part_2(input: &[String]) -> i64 {
                             }
 
                             // This rectangle can be formed
-                            return calculate_area(&point, &other_point);
+                            return calculate_area(&point1, &point2);
                         }
-                    } else if seen_points.contains(&pair2) {
+                    } else if seen_points.contains(&point4) {
                         // See if there is some point further away than the other pair
-                        if (pair1.x > pair2.x
-                            && pair1.y > pair2.y
-                            && points.iter().any(|&p| p.x >= pair1.x && p.y >= pair1.y))
-                            || (pair1.x > pair2.x
-                                && pair1.y < pair2.y
-                                && points.iter().any(|&p| p.x >= pair1.x && p.y <= pair1.y))
-                            || (pair1.x < pair2.x
-                                && pair1.y > pair2.y
-                                && points.iter().any(|&p| p.x <= pair1.x && p.y >= pair1.y))
-                            || (pair1.x < pair2.x
-                                && pair1.y < pair2.y
-                                && points.iter().any(|&p| p.x <= pair1.x && p.y <= pair1.y))
+                        if (point3.x > point4.x
+                            && point3.y > point4.y
+                            && points.iter().any(|&p| p.x >= point3.x && p.y >= point3.y))
+                            || (point3.x > point4.x
+                                && point3.y < point4.y
+                                && points.iter().any(|&p| p.x >= point3.x && p.y <= point3.y))
+                            || (point3.x < point4.x
+                                && point3.y > point4.y
+                                && points.iter().any(|&p| p.x <= point3.x && p.y >= point3.y))
+                            || (point3.x < point4.x
+                                && point3.y < point4.y
+                                && points.iter().any(|&p| p.x <= point3.x && p.y <= point3.y))
                         {
                             // Check if there are any points within the rectangle formed
-                            let x_range = pair1.x.min(pair2.x) + 1..pair1.x.max(pair2.x);
-                            let y_range = pair1.y.min(pair2.y) + 1..pair1.y.max(pair2.y);
+                            let (x_range, y_range) = get_ranges(&point3, &point4);
                             if points
                                 .iter()
                                 .any(|&check_point| within_range(&x_range, &y_range, &check_point))
@@ -112,7 +110,7 @@ fn part_2(input: &[String]) -> i64 {
                             }
 
                             // This rectangle can be formed
-                            return calculate_area(&point, &other_point);
+                            return calculate_area(&point1, &point2);
                         }
                     }
                     -1
@@ -146,6 +144,13 @@ fn calculate_area(point1: &Point, point2: &Point) -> i64 {
     let dx = point1.x.abs_diff(point2.x);
     let dy = point1.y.abs_diff(point2.y);
     ((dx + 1) * (dy + 1)) as i64
+}
+
+fn get_ranges(point1: &Point, point2: &Point) -> (std::ops::Range<i64>, std::ops::Range<i64>) {
+    (
+        point1.x.min(point2.x) + 1..point1.x.max(point2.x),
+        point1.y.min(point2.y) + 1..point1.y.max(point2.y),
+    )
 }
 
 fn within_range(

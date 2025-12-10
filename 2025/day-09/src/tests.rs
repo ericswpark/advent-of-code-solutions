7b4706e904e08eabbdff33ec8c99afc0c02baf5a
Eric Park <me@ericswpark.com>
2025-12-09 23:25:55 -0500

    2025: day 9: attempt part 2
    Too slow

--- 2025/day-09/src/tests.rs
+++ 2025/day-09/src/tests.rs
@@ -7,7 +7,7 @@ mod tests {
     fn sample_input() {
         let input = get_input("sample-input.txt");
         assert_eq!(part_1(&input), 50);
-        //assert_eq!(part_2(&input), 25272);
+        assert_eq!(part_2(&input), 24);
     }
 
     #[test]

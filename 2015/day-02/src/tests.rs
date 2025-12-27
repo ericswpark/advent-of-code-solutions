eec6eba40d21cd379eef64bfc66b0c0aa181ffbd
Eric Park <me@ericswpark.com>
2025-12-27 22:04:34 +0900

    2015: day 2: finish part 1

--- 2015/day-02/src/tests.rs
+++ 2015/day-02/src/tests.rs
@@ -18,7 +18,7 @@ mod tests {
     #[test]
     fn puzzle_input() {
         let input = get_input("puzzle-input.txt");
-        // assert_eq!(part_1(&input), 232);
+        assert_eq!(part_1(&input), 1598415);
         // assert_eq!(part_2(&input), 1783);
     }
 }

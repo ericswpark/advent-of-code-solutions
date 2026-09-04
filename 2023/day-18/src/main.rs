819eb8add9d31ffd452a110a42119f0025a0df79
Eric Park <me@ericswpark.com>
2026-09-03 20:36:44 -0400

    2023: day 18: setup

--- 2023/day-18/src/main.rs
+++ 2023/day-18/src/main.rs
@@ -1,3 +1,21 @@
+mod tests;
+
+use helpers::*;
+
 fn main() {
-    println!("Hello, world!");
+    let input = get_input(&get_path_from_arg());
+
+    let part_1_answer = part_1(&input);
+    println!("Part 1 answer: {part_1_answer}");
+
+    let part_2_answer = part_2(&input);
+    println!("Part 2 answer: {part_2_answer}");
+}
+
+fn part_1(input: &Vec<String>) -> i64 {
+    todo!();
+}
+
+fn part_2(input: &Vec<String>) -> i64 {
+    todo!();
 }
